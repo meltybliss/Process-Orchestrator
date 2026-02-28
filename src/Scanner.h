635895f4bd9ac6345b79364d1d716eee546f9a1e
melty <Yusei.k.0403@gmail.com>
2026-03-01 00:00:55 +0900

    introduced next scan with 2 pointer method

--- src/Scanner.h
+++ src/Scanner.h
@@ -31,24 +31,50 @@ private:
 				//一気にbufferにbaseAddressからそのregionの終わりまで読み込む
 				if (ReadProcessMemory(proc.hProcess, mbi.BaseAddress, buffer.data(), mbi.RegionSize, &bytesRead)) {
 					for (size_t i = 0; i <= bytesRead - sizeof(T); i += sizeof(T)) {
-						T value = *(T*)&buffer[i];
+						T value = *(T*)&buffer[i];//1バイトしか指せないはずの場所（char）を、一度アドレス（ポインタ）に変換することで、そこからNバイト分（T）まで視界を広げる
 
 						if (value == targetValue) {
-							uintptr_t foundAddr = (uintptr_t)mbi.BaseAddress + i;
+							ScanResult result;
+							result.addr = (uintptr_t)mbi.BaseAddress + i;
+							*(T*)result.lastValue = value;
 
-							pool.write(foundAddr);
+							pool.write(result);
 						}
-					} 
+					}
 				}
-
+			}
 
 			addr = (uintptr_t)mbi.BaseAddress + mbi.RegionSize;//次のメモリの塊へ移動
 
 		}
 
+	}
 
 
+	template<typename T>
+	void nextSearch(Process & proc, T targetVal) {
+		ScanResult* readPtr = pool.targetBasePtr;
 
-	};
+		ScanResult* writePtr = readPtr;
+
+		ScanResult* endPtr = pool.targetBasePtr + pool.offset;//終端ポインタ
+
+		while (readPtr < endPtr) {
+			T curVal;
+
+			if (proc.Read(readPtr->addr, curVal)) {
+				if (curVal == targetVal) {
+					*(T*)readPtr->lastValue = curVal;
+					*writePtr = *readPtr;
+					writePtr++;
+				}
+			}
+
+			readPtr++;
+
+		}
+
+		pool.offset = (size_t)(writePtr - pool.targetBasePtr);//ポインタ同士を引き算すると、バイト数ではなく**「その型（ScanResult）が何個分入るか
+	}
 
 };
